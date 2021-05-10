0b0a7a482c5c4a0a25c3a48e9e0bd7d95882a8db
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-10 13:47:04 +0300

    2 task/1 mod

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
+let productName = "Droid";
+let pricePerItem = 2000;
+
 // Change code below this line
 
-
-const productName = "Droid";
-const pricePerItem = 2000;
-// console.log(productName);
-// 'Droid'
-
-// console.log(pricePerItem);
-// 2000
+productName = "Repair droid";
+pricePerItem = 3500;
