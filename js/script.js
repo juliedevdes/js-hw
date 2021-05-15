ec4d180665f0ce78e3731442a026118e65c98ea4
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-15 18:52:12 +0300

    32/1mod
    задача с длиной строки

--- js/script.js
+++ js/script.js
@@ -1,46 +1,14 @@
-function getShippingCost(country) {
-    let message;
-    // Change code below this line
-    let price;
-    switch (country) {
-        case "China":
-            price = 100;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
+function getSubstring(string, length) {
+    const substring = (string.slice(string, length)); // Change this line
 
-        case "Chile":
-            price = 250;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-
-        case "Australia":
-            price = 170;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-
-        case "Jamaica":
-            price = 120;
-            message = `Shipping to ${country} will cost ${price} credits`;
-            break;
-
-        default:
-            message = "Sorry, there is no delivery to your country";
-    }
-
-    // Change code above this line
-    return message;
+    return substring;
 }
+/* Метод строк slice(startIndex, endIndex) используется для создания копии части или всей строки. 
+Он делает копию элементов строки от startIndex и до, но не включая endIndex и возвращает новую строку.
 
+Функция getSubstring(string, length) принимает строку и возвращает подстроку от начала и до length символов. Она объявляет два параметра, значения которых будут задаваться во время её вызова:
 
-/* Функция getShippingCost(country) должна проверять возможность доставки товара в страну пользователя (параметр country) и возвращать сообщение о результате хранящееся в переменной message. Обязательно используй инструкцию switch.
-
-Формат возвращаемой строки "Shipping to <country> will cost <price> credits", где вместо <country> и <price> необходимо подставить соотвествующие значения.
-
-Список стран и стоимость доставки:
-
-China - 100 кредитов
-Chile - 250 кредитов
-Australia - 170 кредитов
-Jamaica - 120 кредитов
-Из списка видно, что доставка есть не везде. Если указанной страны нет в списке, то функция должна вернуть строку "Sorry, there is no delivery to your country"
+string - оригинальная строка
+length - количество символов с начала строки для подстроки
+Присвой переменной substring выражение создания подстроки длинной length символов (от начала) из строки string.
 */
