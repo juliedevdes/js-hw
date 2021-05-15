3b2dbb59fba7c2bfdc47533d4b4d6bf8f7446215
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-15 16:51:47 +0300

    24/mod 1
    discount exersize

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-function checkIfCanAccessContent(subType) {
-    const canAccessContent = (subType === "pro" || subType === "vip"); // Change this line
-
-    return canAccessContent;
+function getDiscount(totalSpent) {
+    const BASE_DISCOUNT = 0;
+    const BRONZE_DISCOUNT = 0.02;
+    const SILVER_DISCOUNT = 0.05;
+    const GOLD_DISCOUNT = 0.1;
+    let discount;
+    // Change code below this line
+    if (totalSpent >= 50000) {
+        discount = GOLD_DISCOUNT;
+    } else if (totalSpent >= 20000 && totalSpent < 50000) {
+        discount = SILVER_DISCOUNT;
+    } else if (totalSpent >= 5000 && totalSpent < 20000) {
+        discount = BRONZE_DISCOUNT;
+    } else {
+        discount = BASE_DISCOUNT;
+    }
+    // Change code above this line
+    return discount;
 }
-/* Оператор || приводит все операнды к булю и возвращает значение одного из них. Левый операнд, если его можно привести к true, и правый в остальных случаях.
+/* Функция getDiscount(totalSpent) определяет значение скидки в зависимости от общей суммы потраченных денег (параметр totalSpent) 
+в магазине за всё время (партнёрская программа). Скидка записывается в переменную discount и возвращается из функции как результат её работы.
 
-Функция checkIfCanAccessContent(subType) проверяет, может ли пользователь получить доступ к контенту. Проверка происходит по типу подписки.
-Получить доступ могут только пользователи с подпиской pro или vip.
+Используя ветвления и логические операторы, дополни код функции.
 
-Присвой переменной canAccessContent выражение проверки подписки. Если значение параметра subType равно строкам "pro" или "vip",
-пользователь получит доступ. Результатом выражения проверки будет буль true или false.
+Если потрачено от 50000 ( включительно ) или больше кредитов - скидка 10% (золотой партнёр)
+Если потрачено от 20000 (включительно) до 50000 кредитов - скидка 5% (серебрянный партнёр)
+Если потрачено от 5000 (включительно) до 20000 кредитов - скидка 2% (бронзовый партнёр)
+Если потрачено меньше чем 5000 кредитов - скидка 0 (базовый партнёр)
+Значения скидок каждого уровня хранятся в одноимённых константах BASE_DISCOUNT, BRONZE_DISCOUNT, SILVER_DISCOUNT и GOLD_DISCOUNT.
 
-Тесты
+
 */
