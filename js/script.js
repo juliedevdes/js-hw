7481d192282157ab90e7f342c2bb976bef4b48cc
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-15 17:03:49 +0300

    25/1mod

--- js/script.js
+++ js/script.js
@@ -1,32 +1,28 @@
-function getDiscount(totalSpent) {
-    const BASE_DISCOUNT = 0;
-    const BRONZE_DISCOUNT = 0.02;
-    const SILVER_DISCOUNT = 0.05;
-    const GOLD_DISCOUNT = 0.1;
-    let discount;
-    // Change code below this line
-    if (totalSpent >= 50000) {
-        discount = GOLD_DISCOUNT;
-    } else if (totalSpent >= 20000 && totalSpent < 50000) {
-        discount = SILVER_DISCOUNT;
-    } else if (totalSpent >= 5000 && totalSpent < 20000) {
-        discount = BRONZE_DISCOUNT;
-    } else {
-        discount = BASE_DISCOUNT;
-    }
-    // Change code above this line
-    return discount;
+/*function checkStorage(available, ordered) {
+  let message;
+  // Change code below this line
+
+  if (ordered > available) {
+    message = "Not enough goods in stock!";
+  } else {
+    message = "The order is placed, the manager will contact you";
+  }
+
+  // Change code above this line
+  return message;
 }
-/* Функция getDiscount(totalSpent) определяет значение скидки в зависимости от общей суммы потраченных денег (параметр totalSpent) 
-в магазине за всё время (партнёрская программа). Скидка записывается в переменную discount и возвращается из функции как результат её работы.
+*/
 
-Используя ветвления и логические операторы, дополни код функции.
 
-Если потрачено от 50000 ( включительно ) или больше кредитов - скидка 10% (золотой партнёр)
-Если потрачено от 20000 (включительно) до 50000 кредитов - скидка 5% (серебрянный партнёр)
-Если потрачено от 5000 (включительно) до 20000 кредитов - скидка 2% (бронзовый партнёр)
-Если потрачено меньше чем 5000 кредитов - скидка 0 (базовый партнёр)
-Значения скидок каждого уровня хранятся в одноимённых константах BASE_DISCOUNT, BRONZE_DISCOUNT, SILVER_DISCOUNT и GOLD_DISCOUNT.
 
+function checkStorage(available, ordered) {
+    let message;
+    // Change code below this line
+    message = ordered <= available ? "The order is accepted, our manager will contact you" : "Not enough goods in stock!";
+    // Change code above this line
+    return message;
+}
+
+/* Тернарный оператор используется как более синтаксически краткая замена инструкции if...else, когда одной и той же переменной необходимо присвоить разные значения по условию.
 
 */
