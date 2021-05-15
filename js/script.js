2074d87f467d2593e5c21a61f00d0b7658f9ceac
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-15 17:07:42 +0300

    26/1mod
    task with ternar operator

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
-/*function checkStorage(available, ordered) {
-  let message;
-  // Change code below this line
-
-  if (ordered > available) {
-    message = "Not enough goods in stock!";
-  } else {
-    message = "The order is placed, the manager will contact you";
-  }
-
-  // Change code above this line
-  return message;
-}
-*/
-
-
-
-function checkStorage(available, ordered) {
+function checkPassword(password) {
+    const ADMIN_PASSWORD = "jqueryismyjam";
     let message;
     // Change code below this line
-    message = ordered <= available ? "The order is accepted, our manager will contact you" : "Not enough goods in stock!";
+    message = password === ADMIN_PASSWORD ? "Access is allowed" : "Access denied, wrong password!";
+
     // Change code above this line
     return message;
 }
+
+
 
 /* Тернарный оператор используется как более синтаксически краткая замена инструкции if...else, когда одной и той же переменной необходимо присвоить разные значения по условию.
 
