1144fac00099bee85baea60d85b13eca48d7d6b3
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-18 21:10:00 +0300

    4

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const fruits = ["apple", "plum", "pear", "orange"];
+
+// Change code below this line
+const firstElement = fruits[0];
+const secondElement = fruits[1];
+const lastElement = fruits[fruits.length - 1];
+
+/*Доступ к элементам по индексу
+Для доступа к значению элемента массива применяют синтаксис квадратных скобок массив[индекс]. Между именем переменной массива и квадратными скобками не должно быть пробела.
+
+Внимание
+Индексация элементов массива начинается с нуля.
+
+const planets = ["Earth", "Mars", "Venus"];
+planets[0]; // "Earth"
+planets[2]; // "Venus"
+
+
+Задание
+Объяви три переменные и присвой каждой из них значение, используя нотацию квадратных скобок.
+
+Имя переменной	Значение переменной
+firstElement	первый элемент массива
+secondElement	второй элемент массива
+lastElement	последний элемент массива*/
