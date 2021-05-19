f0f98af2a1ed3774d1bb381ea3d9b33a7c4adf3c
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-19 22:29:01 +0300

    13 slug generator

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-function makeStringFromArray(array, delimeter) {
-    let string;
+function slugify(title) {
     // Change code below this line
-
-
-    string = array.join(delimeter);
+    const normtitle = title.toLowerCase();
+    const titlesp = normtitle.split(" ");
+    const slug = normtitle.join('-');
+    return slug;
     // Change code above this line
-    return string;
 }
-/*Метод массива join()
-Метод массивов join(delimeter) позволяет соединить элементы массива в строку. В строке элементы будут разделены символом или группой символов указанных в delimeter. То есть это операция обратная методу строк split(delimeter).
 
-const words = ["JavaScript", "is", "amazing"];
-console.log(words.join("")); // 'JavaScriptisamazing'
-console.log(words.join(" ")); // 'JavaScript is amazing'
-console.log(words.join("*")); // 'JavaScript*is*amazing' */
+
+const result = slugify("I want sleep");
+
+console.log(result);
+/*Задача: генератор slug
+Задание
+Термин slug - это человеко-понятный уникальный идентификатор, который используется в веб-разработке для создания читабельных URL-адесов.
+
+Например, вместо того чтобы пользователь увидел в адресной строке mysite.com/posts/1q8fh74tx, можно сделать slug из названия статьи. В результате адрес получится более приятным для восприятия: mysite.com/posts/arrays-for-begginers.
+
+Внимание
+Slug это всегда строка в нижнем регистре, слова которой разделены тире.
+
+Напиши функцию slugify(title) которая принимает заголовок статьи, параметр title, и возвращает slug, созданный из этой строки.
+
+Значением параметра title будут строки, слова которых разделены только пробелами
+Все символы slug должны быть в нижнем регистре
+Все слова slug должна быть разделены тире*/
