0bee277beaae06a6ab5a42368b4358977a546e23
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-19 21:42:14 +0300

    9

--- js/script.js
+++ js/script.js
@@ -1,16 +1,12 @@
-const fruits = ["apple", "plum", "pear", "orange"];
+function getExtremeElements(array) {
+    const fistEl = array[0];
+    const lastEl = array[array.length - 1];
+    return [fistEl, lastEl];
 
-// Write your code under this line
-fruits[1] = 'peach';
-fruits[3] = 'banana';
-/*В отличии от строк, 
-элементы массива можно изменять !!!
+}
 
-обратившись к ним по индексу и присвоив другое значение.
+const check = getExtremeElements([2, 5, 7]);
+console.log(check);
 
-const numbers = [1, 2, 3, 4, 5];
-numbers[0] = 7;
-numbers[2] = 14;
-console.log(numbers); // [7, 2, 14, 4, 5];
-Задание
-Переопредели значения элементов с индексами 1 и 3. Замени "plum" на "peach", а "orange" на "banana".*/
+/*Напиши функцию getExtremeElements(array) которая принимает один параметр array - массив элементов произвольной длины. 
+Функция должна возвращать массив из двух элементов - первого и последнего элемента параметра array. */
