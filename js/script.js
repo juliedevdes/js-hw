01ed953349e97fb61b5679f55c2319f562adcb25
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-22 17:36:39 +0300

    27 рефакторинг - сортировка массива по условию

--- js/script.js
+++ js/script.js
@@ -1,20 +1,46 @@
 /*
 
 ===== ЗАДАНИЕ ======
-Выполни рефакторинг кода функции calculateTotalPrice(order) заменив цикл for на for...of.
-*/
+Зачада: фильтрация массива чисел 2.0
+Задание
+Выполни рефакторинг функции filterArray(numbers, value) заменив цикл for на for...of.*/
 
-function calculateTotalPrice(order) {
-    let total = 0;
-    // Change code below this line
 
-    for (const item of order) {
-        total += item;
+/*старый вариант
+function filterArray(numbers, value) {
+  // Change code below this line
+  const filteredNumbers = [];
+
+  for (let i = 0; i < numbers.length; i += 1) {
+    const number = numbers[i];
+
+    if (number > value) {
+      filteredNumbers.push(number);
     }
+  }
 
-    // Change code above this line
-    return total;
+  return filteredNumbers;
+  // Change code above this line
 }
 
 
-console.log(calculateTotalPrice([12, 12]));
+
+*/
+
+
+function filterArray(numbers, value) {
+    // Change code below this line
+    const filteredNumbers = [];
+    for (const number of numbers) {
+
+        if (number > value) {
+            filteredNumbers.push(number);
+        }
+    }
+
+    return filteredNumbers;
+    // Change code above this line
+}
+
+
+console.log(filterArray([12, 12], 10));
