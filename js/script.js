6793b4ee7e554a274c82beef3e63ea462f696a31
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-22 15:22:44 +0300

    20 задача подсчета суммы чисел в массиве

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-const fruits = ['apple', 'plum', 'pear', 'orange'];
+function calculateTotalPrice(order) {
+    let total = 0;
+    // Change code below this line
+    for (let i = 0; i < order.length; i += 1) {
+        total = total + order[i];
+    }
 
-for (let i = 0; i < fruits.length; i += 1) { // Change this line
-    const fruit = fruits[i]; // Change this line
-    console.log(fruit);
+    /* for (const item of order) {
+        total += item;
+    }*/
+
+    // Change code above this line
+    return total;
 }
+console.log(calculateTotalPrice([70, 40, 60]));
+
+
 /*
-
-Итерация по массиву
-Цикл for можно использовать для итерации по массиву, то есть «перебрать» его поэлементно.
-
-const planets = ["Earth", "Mars", "Venus"];
-
-for (let i = 0; i < planets.length; i += 1) {
-  console.log(planets[i]);
-}
-Для доступа к элементам используется синтаксис квадратных скобок массив[индекс], 
-где индекс - это значение счётчика цикла от 0 и до последнего индекса массива, который на единицу меньше длины массива.
+Напиши функцию calculateTotalPrice(order), которая принимает один параметр order - массив чисел, и рассчитывает общую сумму его элементов.
+ Общая сумма элементов должна сохраняться в переменной total, которая возвращается, как результат работы функции.
 
 */
