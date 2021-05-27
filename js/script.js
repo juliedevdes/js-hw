73b13f28d6462b274dd53a18e003113ac2042fed
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-27 16:09:51 +0300

    13 подсчет свойст

--- js/script.js
+++ js/script.js
@@ -1,74 +1,35 @@
 "use strict";
 
+/// ======Задача подсчет свойств====
 
-const keys = [];
-const values = [];
-const advert = {
-    service: 'apt',
-};
-const apartment = Object.create(advert);
-apartment.descr = 'Spacious apartment in the city centre';
-apartment.rating = 4;
-apartment.price = 2153;
 
-for (const key in apartment) {
+/*
+Задание
+Напиши функцию countProps(object), которая считает и возвращает количество собственных свойств объекта в параметре object.
+Используй переменную propCount для хранения количества свойств объекта.
+
+*/
+
+function countProps(object) {
+    let propCount = 0;
     // Change code below this line
-    if (apartment.hasOwnProperty(key)) {
-        keys.push(key);
-        values.push(apartment[key]);
+    for (const key in object) {
+        if (object.hasOwnProperty(key)) {
+            propCount += 1;
+        }
     }
     // Change code above this line
+    return propCount;
 }
-
-console.log(values);
-console.log(keys);
-console.log(apartment.hasOwnProperty("rating"));
-
+console.log(countProps({ name: "Mango", age: 2 }));
 
 /* 
+1 перебрать свойства объкта фор ин 
 
-Метод hasOwnProperty()
-Разберём концепцию собственных и несобственных свойств объекта и научимся правильно использовать цикл for...in.
+2 поставить условие - если true hasOwnObjects. дейтсвительно собственное свойство или нет
 
-const animal = {
-  legs: 4,
-};
-const dog = Object.create(animal);
-dog.name = 'Манго';
+3 если да - добавить в переменную 1
 
-console.log(dog); // {name: 'Манго'}
-console.log(dog.name); // 'Манго'
-console.log(dog.legs); // 4
-Метод Object.create(animal) создаёт и возвращает новый объект, связывая его с объектом animal. Поэтому можно получить значение свойства legs обратившись к нему как dog.legs, хотя его нет в объекте dog - это несобственное свойство из объекта animal.
-
-Оператор in, который используется в цикле for...in, не делает различия между собственными и несобственными свойствами объекта. Эта особенность мешает, так как мы всегда хотим перебрать только собственные свойства. Для того чтобы узнать есть в объекте собственное свойство или нет, используется метод hasOwnProperty(key), который возвращает true или false.
-
-//  Возвращает true для всех свойств
-console.log('name' in dog); // true
-console.log('legs' in dog); // true
-
-//  Возвращает true только для собственных свойств
-console.log(dog.hasOwnProperty('name')); // true
-console.log(dog.hasOwnProperty('legs')); // false
-Поэтому при переборе циклом for...in необходимо на каждой итерации добавить проверку на собственное свойство. Даже если сейчас мы уверены в том что у объекта нет несобственных свойств, это оградит от возможных ошибок в будущем.
-
-const book = {
-  title: 'Последнее королевство',
-  author: 'Бернард Корнуэлл',
-  genres: ['историческая проза', 'приключения'],
-  rating: 8.38,
-};
-
-for (const key in book) {
-  // Если это собственное свойство - выполняем тело if
-  if (book.hasOwnProperty(key)) {
-    console.log(key);
-    console.log(book[key]);
-  }
-
-  // Если это не собственное свойство - ничего не делаем
-}
-Задание
-Выполни рефакторинг решения предыдущего задания добавив в цикл for...in проверку на собственное свойство.
+4 вернуть переменную с подсчетом
 
 */
