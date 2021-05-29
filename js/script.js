3d877b79ffaccd41433d877b4f8b8d3500c4596c
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-29 21:25:23 +0300

    19

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 "use strict";
 
-/*Задача. Поиск объекта по значению свойства
+/*====== Коллекция значений свойства ======
+
 Задание
-Напиши функцию getProductPrice(productName) которая принимает один параметр productName - название продукта. 
-Функция ищет объект продукта с таким именем (свойство name) в массиве products и возвращает его цену (свойство price). 
-Если продукт с таким названием не найден, функция должна возвращать null.
- */
+
+Напиши функцию getAllPropValues(propName) которая :
+►  принимает один параметр propName - имя (ключ) свойства. 
+►  должна вернуть массив всех значений свойства с таким именем из каждого объекта в массиве products. 
+►  eсли в объектах нет свойства с таким именем, функция должна вернуть пустой массив.*/
+
+
 const products = [
     { name: 'Radar', price: 1300, quantity: 4 },
     { name: 'Scanner', price: 2700, quantity: 3 },
@@ -13,29 +17,49 @@ const products = [
     { name: 'Grip', price: 1200, quantity: 9 },
 ];
 
-function getProductPrice(productName) {
-    // Change code below this line
 
-    for (const product of products) {
-        if (product.name === productName) {
-            return product.price;
+/// ===== РЕШЕНИЕ #1 =======
+// function getAllPropValues(propName) {
+//     const array = [];
+//     for (const product of products) { //1 перебираем объекты, как частичка массива. product = { name: 'Radar', price: 1300, quantity: 4 }
+//         const keys = Object.keys(product); // 2 делаем массив с ключей всех объктов в массиве, названий свойст
+//         //   console.log(keys);
+
+//         if (keys.includes(propName)) { // 3 спрашиваем есть ли в массиве свойст запрашиваемое
+//             array.push(product[propName]); // 4 пушим значение в новый массив если да
+//         }
+//     }
+//     return array; // 5 возвращаем новый массив
+// }
+
+// ====== решение №2, покороче
+function getAllPropValues(propName) {
+    const propValues = []; // 1 массив, который будем возвращать
+    for (const product of products) { // 2 перебираем объкты в массиве
+        for (const key in product) { // 3 перебираем свойтства в объектах
+            if (key == propName) { // 4 условие если свойство = задаваемое значение
+                propValues.push(product[propName]); /// 5 .. то пушим в новый массив
+            }
         }
     }
-    return null;
-
-    // Change code above this line
+    return propValues;
 }
 
-console.log(getProductPrice("Scanner"));
-/**
- *  1 перебрать массив объктов сравнивая свойсто имени с именем в функции
- *
- *  2 условие выполнено совпадает имя ► вернуть цену, нет ► ноль
- * 
- *  3 проверить
- * 
- * 
- * 
- * 
- * 
- */
+
+
+console.log(getAllPropValues("price"));
+console.log(getAllPropValues("тфьу"));
+console.log(getAllPropValues("name"));
+
+
+
+// const object = {
+//     name: 12,
+//     age: 13,
+// };
+
+// for (const key in object) {
+//     console.log(object['name']);
+
+
+// }
