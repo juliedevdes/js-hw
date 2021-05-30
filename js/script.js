796f7f3229a11a4097572f6ccd5c4b02ad29f3f7
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-30 16:34:47 +0300

    28

--- js/script.js
+++ js/script.js
@@ -1,28 +1,13 @@
 "use strict";
 
-/*======Паттерн «Объект настроек» ======
 
 
-/*
 
-Если функция принимает более двух-трёх аргументов, очень просто запутаться в какой последовательности что передавать. 
-В результате получается очень неочевидный код в месте её вызова.
-
- */
-
-
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
 // Change code below this line
-function calculateMeanTemperature(forecast) {
-    const {
-        today: { low: todayLow, high: todayHigh },
-        tomorrow: { low: tomorrowLow, high: tomorrowHigh }
-    } = forecast;
-
-    // const todayLow = forecast.today.low;
-    // const todayHigh = forecast.today.high;
-    // const tomorrowLow = forecast.tomorrow.low;
-    // const tomorrowHigh = forecast.tomorrow.high;
-
-    // Change code above this line
-    return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
-}
+const allScores = [...firstGroupScores, ...secondGroupScores, ...thirdGroupScores];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+console.log(bestScore);
