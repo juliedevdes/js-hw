cad221ea33d5b22db1fa08a3ce3153ef9da6e89f
Julia Ohrimenko <juliajam2497@gmail.com>
2021-05-30 21:24:00 +0300

    33

--- js/script.js
+++ js/script.js
@@ -1,19 +1,24 @@
 "use strict";
 
 /** =====Задача=====
-Функция addOverNum() считает сумму всех аргументов. 
-Измени параметры и тело функции addOverNum() так, чтобы она считала сумму только тех аргументов, которые больше чем заданное число. Это число должно быть первым параметром функции.*/
+Задача. Массив совпадений
+Задание
+Функция findMatches() принимает произвольное количество аргументов. Первым аргументом всегда будет массив чисел, а остальные аргументы будут просто числами.
 
+Дополни код функции так, чтобы она возвращала новый массив matches, в котором будут только те аргументы, начиная со второго, которые есть в массиве первого аргумента.
 
+Например, findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7) должна вернуть массив [1, 2], потому что только они есть в массиве первого аргумента.*/
 
-function addOverNum(number, ...args) {
-    let total = 0;
-
-    for (const arg of args) {
-        if (arg > number) {
-            total += arg;
+// Change code below this line
+function findMatches(array, ...rest) {
+    const matches = []; // Don't change this line
+    for (const item of rest) {
+        if (array.includes(item)) {
+            matches.push(item);
         }
     }
-    return total;
     // Change code above this line
+    return matches;
 }
+
+console.log(findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7));
