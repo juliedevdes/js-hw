64f75129eeaa202fbecae9b68b6d76c4cbfd71cd
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-02 19:02:18 +0300

    9 Метод bind

--- js/script.js
+++ js/script.js
@@ -1,46 +1,64 @@
 // === TASK ==== 
 /**
- *   ===  Метод call
-Бывают ситуации когда функцию нужно вызвать в контексте какого-то объекта, при этом функция не является его методом. Для этого у функций есть методы call и apply.
+ *   === Метод bind
+Методы call и apply вызывают функцию «на месте», то есть сразу. Но в случае колбэк-функций, когда необходимо не сразу вызвать функцию, а передать ссылку на неё, причём с привязанным контекстом, используется метод bind.
+
+foo.bind(obj, arg1, arg2, ...)
+Метод bind создаёт и возвращает копию функции foo с привязанным контекстом obj и аргументами arg1, arg2 и т. д. Получается копия функции которую можно передать куда угодно и вызвать когда угодно.
+
+function greet(clientName) {
+  return `${clientName}, добро пожаловать в «${this.service}».`;
+}
+
+const steam = { service: 'Steam' };
+const steamGreeter = greet.bind(steam);
+steamGreeter('Манго'); // "Манго, добро пожаловать в «Steam»."
+
+const gmail = { service: 'Gmail' };
+const gmailGreeter = greet.bind(gmail);
+gmailGreeter('Поли'); // "Поли, добро пожаловать в «Gmail»."
+Задание
+Функция composeMessage(customerName) создаёт приветственные сообщения для ресторанов. 
+Дополни код так, чтобы в переменных pizzaPalaceComposer и burgerShackComposer были её копии с привязанным контекстом к соответствующим объектам.*/
 
 
+const pizzaPalace = {
+    company: 'Pizza Palace',
+};
 
+const burgerShack = {
+    company: 'Burger Shack',
+};
 
- * Задание
-Сервису приготовления и доставки еды требуется функция генерации сообщений о статусе заказа.
-
-Дополни функцию composeMessage(position) так, чтобы она возвращала строку в формате 'Готовим <блюдо> для <почта>. Ваш заказ <позиция>-й в очереди.' 
-Позиция это значение параметра position - позиция элемента в массиве (на единицу больше чем индекс).
-
-Не объявляй дополнительные параметры функции composeMessage(position).
-Используй call для вызова функции в контексте одного объекта-заказа.
-Используй this в теле функции для доступа к свойствам объекта-заказа в контексте которого она была вызывана.
-Дополни код так, чтобы в переменной messages получился массив сообщений о статусе заказов из массива orders с помощью цикла for.*/
-
-
-const orders = [
-    { email: "solomon@topmail.ua", dish: "Burger" },
-    { email: "artemis@coldmail.net", dish: "Pizza" },
-    { email: "jacob@mail.com", dish: "Taco" },
-];
+function composeMessage(customerName) {
+    return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+}
 // Пиши код ниже этой строки
-function composeMessage(position) {
-    return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
-}
-const messages = [];
-for (let i = 0; i < orders.length; i += 1) {
-    messages.push(composeMessage.call(orders[i], i + 1));
-}
+
+const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+const burgerShackComposer = composeMessage.bind(burgerShack);
+const burgerShackMessage = burgerShackComposer('Поли');
+
 
 // исходный
 //
-// const orders = [
-//     { email: 'solomon@topmail.ua', dish: 'Burger' },
-//     { email: 'artemis@coldmail.net', dish: 'Pizza' },
-//     { email: 'jacob@mail.com', dish: 'Taco' },
-// ];
+// const pizzaPalace = {
+//   company: 'Pizza Palace',
+// };
 
+// const burgerShack = {
+//   company: 'Burger Shack',
+// };
+
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
 // // Пиши код ниже этой строки
-// function composeMessage(position) {}
 
-// const messages = [];
+// const pizzaPalaceComposer = composeMessage;
+// const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+// const burgerShackComposer = composeMessage;
+// const burgerShackMessage = burgerShackComposer('Поли');
