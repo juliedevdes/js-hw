8b1f3315dbbc8d38518a94c9c5eca9ad4a5062c1
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-09 22:33:07 +0300

    7 Конструктор строк

--- js/script.js
+++ js/script.js
@@ -1,47 +1,37 @@
 // === TASK ==== 
 /**
  * 
- * С помощью Function Declaration напиши функцию-конструктор Storage, которая будет создавать объекты для управления складом товаров. Функция ожидает только один аргумент - начальный массив товаров, который записывается на создаваемый объект в свойство items.
+Конструктор строк
+Задание
+С помощью Function Declaration напиши функцию-конструктор StringBuilder, которая принимает один параметр baseValue - 
+произвольную строку, которая записывается на создаваемый объект в свойство value.
 
 Добавь методы на прототип:
 
-getItems() - возвращает массив текущих товаров в свойстве items объекта, который вызывает этот метод.
-addItem(newItem) - принимает новый товар newItem и добавляет его в массив товаров в свойстве items объекта, который вызывает этот метод.
-removeItem(item) - принимает товар item и удаляет его из массива товаров в свойстве items объекта, который вызывает этот метод.
+getValue() - возвращает текущее значение свойства value.
+padEnd(str) - получает парметр str (строку) и добавляет её в конец значения свойства value объекта, который вызывает этот метод.
+padStart(str) - получает парметр str (строку) и добавляет её в начало значения свойства value объекта, который вызывает этот метод.
+padBoth(str) - получает парметр str (строку) и добавляет её в начало и в конец значения свойства value объекта, который вызывает этот метод.
 Под комментарием мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
- * 
- * function Car({ brand, model, price }) {
-  this.brand = brand;
-  this.model = model;
-  this.price = price;
-
-  
-const messages = [];
-for (let i = 0; i < orders.length; i += 1) {
-    messages.push(composeMessage.call(orders[i], i + 1));
-}
- * 
  */
 
+function StringBuilder(baseValue) { this.value = baseValue };
 
-function Storage(items) { this.items = items };
-Storage.prototype.getItems = function() { return this.items; };
+StringBuilder.prototype.getValue = function() { return this.value };
 
-Storage.prototype.addItem = function(newItem) { this.items.push(newItem); };
+StringBuilder.prototype.padEnd = function(str) { this.value += str };
 
-Storage.prototype.removeItem = function(item) {
-    for (let i = 0; i < this.items.length; i += 1) {
-        if (this.items[i] === item) {
-            this.items.splice(i, 1);
-        }
-    }
-};
+StringBuilder.prototype.padStart = function(str) { this.value = str + this.value };
+
+StringBuilder.prototype.padBoth = function(str) { this.value = str + this.value + str };
 
 
 // Пиши код выше этой строки
-const storage = new Storage(['Нанитоиды', 'Пролонгер', 'Антигравитатор']);
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
-storage.addItem('Дроид');
-console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
-storage.removeItem('Пролонгер');
-console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // '.'
+builder.padStart('^');
+console.log(builder.getValue()); // '^.'
+builder.padEnd('^');
+console.log(builder.getValue()); // '^.^'
+builder.padBoth('=');
+console.log(builder.getValue()); // '=^.^='
