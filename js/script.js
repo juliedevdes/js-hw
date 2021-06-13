20653e2ac4875c342bb3b1b2abf759f7cc435c54
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-13 15:56:37 +0300

    19 Методы дочернего класса

--- js/script.js
+++ js/script.js
@@ -1,44 +1,56 @@
 // === TASK ===
 
 // Задание
-// Добавь классу Admin метод constructor, который принимает один параметр - объект настроек с двумя свойствами email и accessLevel. Добавь классу Admin публичное свойство accessLevel, значение которого будет передаваться при вызове конструктора.
+// Добавь классу Admin следующие свойства и методы.
 
-// Чтобы показать как будет использоваться класс Admin, мы добавили инициализацию экземпляра под объявлением класса.
+// Публичное свойство blacklistedEmails для хранения чёрного списка почтовых адресов пользователей. Значение по умолчанию это пустой массив.
+// Публичный метод blacklist(email) для добавления почты в чёрный список. Метод должен добавлять значение параметра email в массив хранящийся в свойстве blacklistedEmails.
+// Публичный метод isBlacklisted(email) для проверки почты в чёрном списке. Метод должен проверять наличие значения параметра email в массиве хранящемся в свойстве blacklistedEmails и возвращать true или false.
+// После объявления класса мы добавили инициализацию экземпляра и вызовы методов в той последовательности, в которой твой код будут проверять тесты. Пожалуйста ничего там не меняй.
+
 class User {
   email;
-
   constructor(email) {
     this.email = email;
   }
-
   get email() {
     return this.email;
   }
-
   set email(newEmail) {
     this.email = newEmail;
   }
 }
-
 class Admin extends User {
   // Пиши код ниже этой строки
-  accessLevel;
   static AccessLevel = {
     BASIC: "basic",
     SUPERUSER: "superuser",
   };
-
-  constructor({ email, accessLevel }) {
+  accessLevel;
+  blacklistedEmails;
+  constructor({ email, accessLevel, blacklistedEmails = [] }) {
     super(email);
     this.accessLevel = accessLevel;
+    this.blacklistedEmails = blacklistedEmails;
+  }
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+  isBlacklisted(email) {
+    if (this.blacklistedEmails.includes(email)) {
+      return true;
+    }
+    return false;
   }
   // Пиши код выше этой строки
 }
-
 const mango = new Admin({
   email: "mango@mail.com",
   accessLevel: Admin.AccessLevel.SUPERUSER,
 });
-
 console.log(mango.email); // mango@mail.com
 console.log(mango.accessLevel); // superuser
+mango.blacklist("poly@mail.com");
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted("mango@mail.com")); //  false
+console.log(mango.isBlacklisted("poly@mail.com")); // true
