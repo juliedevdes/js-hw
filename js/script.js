2ee24e141921675de4ab589daaf9e2888473c665
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-17 17:30:19 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Задание
-// Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет. 
+// Дополни код так, чтобы в переменной planetsLengths получился массив длин названий планет.
 // Обязательно используй метод map().
 
 const planets = ["Земля", "Марс", "Венера", "Юпитер"];
 // Пиши код ниже этой строки
-const planetsLengths = planets.map((planet) => planet.length)const planets = ['Земля', 'Марс', 'Венера', 'Юпитер'];
+const planetsLengths = planets.map((planet) => planet.length);
