00b1e544dab38a2021fe20c1a62ff9f58d9ee51a
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-17 16:29:13 +0300

    Метод forEach(callback)

---
+++ js/script.js
@@ -0,0 +1,22 @@
+<<<<<<< Updated upstream
+=======
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Пиши код ниже этой строки
+orderedItems.forEach(item => totalPrice += item);
+  // Пиши код выше этой строки
+  return totalPrice;
+}
+
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Пиши код ниже этой строки
+
+//   for (let i = 0; i < orderedItems.length; i += 1) {
+//     totalPrice += orderedItems[i];
+//   }
+
+//   // Пиши код выше этой строки
+//   return totalPrice;
+// }
+>>>>>>> Stashed changes
