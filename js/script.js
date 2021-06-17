e63ea2df8171def4de42ebe89b2faded6f21ea69
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-17 17:15:16 +0300

    9 clear functions

--- js/script.js
+++ js/script.js
@@ -1,31 +1,30 @@
-// Задача. Фильтрация массива чисел 2.0
 // Задание
-// Замени объявление функции filterArray() и коллбек для метода forEach() на стрелочные функции.
+// Функция changeEven(numbers, value) принимает массив чисел numbers и обновляет каждый элемент,
+// значение которого это чётное число, добавляя к нему значение параметра value.
 
-// Пиши код ниже этой строки
-const filterArray = (numbers, value) => {
-  const filteredNumbers = [];
+// Выполни рефакторинг функции так, чтобы она стала чистой - не изменяла массив чисел numbers,
+//  а создавала, наполняла и возвращала новый массив с обновлёнными значениями.
+
+function changeEven(numbers, value) {
+  // Пиши код ниже этой строки
+  let changedEven = [];
 
   numbers.forEach((number) => {
-    if (number > value) {
-      filteredNumbers.push(number);
+    if (number % 2 === 0) {
+      changedEven.push(number + value);
+    } else {
+      changedEven.push(number);
     }
   });
-
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] % 2 === 0) {
+  //     changedEven.push(numbers[i] + value);
+  //   } else {
+  //     changedEven.push(numbers[i]);
+  //   }
+  // }
+  return changedEven;
   // Пиши код выше этой строки
-  return filteredNumbers;
-};
-
-// // Пиши код ниже этой строки
-// function filterArray(numbers, value) {
-//   const filteredNumbers = [];
-
-//   numbers.forEach(function (number) {
-//     if (number > value) {
-//       filteredNumbers.push(number);
-//     }
-//   });
-
-//   // Пиши код выше этой строки
-//   return filteredNumbers;
-// }
+}
+console.log(changeEven([1, 2, 3, 4, 5], 10));
+// ` возвращает новый массив `[(1, 12, 3, 14, 5)]
