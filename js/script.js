abab4e0463c3bbda2c8129d3513d035cc450be1b
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 16:31:34 +0300

    38 Задача. Сортировка по балансу

--- js/script.js
+++ js/script.js
@@ -1,35 +1,77 @@
 // -----
 
 // ------ TASK ------
-// При работе с массивом объектов сортировка выполняется по числовому или строчному значению какого-то свойства.
-// Например, есть группа студентов с баллами за тест.
-// Необходимо отсортировать массив объектов по возрастанию и убыванию количества баллов, и по имени студента.
-
 // Задание
-// Дополни код так, чтобы:
+// Дополни функцию sortByAscendingBalance(users) так, чтобы она возвращала массив пользователей отсортированный по возрастанию их баланса
+//  (свойство balance).
 
-// В переменной sortedByAuthorName получился массив книг отсортированный по имени автора в алфавитном порядке.
-// В переменной sortedByReversedAuthorName получился массив книг отсортированный по имени автора в обратном алфавитном порядке.
-// В переменной sortedByAscendingRating получился массив книг отсортированный по возрастанию рейтинга.
-// В переменной sortedByDescentingRating получился массив книг отсортированный по убыванию рейтинга.
-
-const books = [
-  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
-  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
-  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
-  { title: "Красна как кровь", author: "Ли Танит", rating: 7.94 },
-  { title: "Враг Божий", author: "Бернард Корнуэлл", rating: 8.67 },
+const testArray = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+  },
 ];
+
 // Пиши код ниже этой строки
-
-const sortedByAuthorName = [...books].sort((a, b) =>
-  a.author.localeCompare(b.author)
-);
-
-const sortedByReversedAuthorName = [...books].sort((a, b) =>
-  b.author.localeCompare(a.author)
-);
-
-const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
-
-const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
+const sortByAscendingBalance = (users) =>
+  [...users].sort((curr, next) => curr.balance - next.balance);
+// Пиши код выше этой строки
