c7ed7f6a924acd1146b1c6c607603f0e5c4ba402
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 17:00:04 +0300

    42 Задача. Пользователи и друзья

--- js/script.js
+++ js/script.js
@@ -2,22 +2,84 @@
 
 // ------ TASK ------
 // Задание
-// Дополни код так, чтобы в переменной names получился массив имён авторов в алфавитном порядке,
-// рейтинг книг которых больше значения переменной MIN_BOOK_RATING.
+// Дополни функцию getNamesSortedByFriendCount(users) так,
+// чтобы она возвращала массив имён пользователей
+// отсортированный по возрастанию количества их друзей (свойство friends
 
-const books = [
-  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
-  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
-  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
-  { title: "Красна как кровь", author: "Ли Танит", rating: 8.14 },
-  { title: "Сны В Ведьмином Доме", author: "Говард Лавкрафт", rating: 8.67 },
+const testArray = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong", "Adrian Cross"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: [
+      "Jacklyn Lucas",
+      "Linda Chapman",
+      "Adrian Cross",
+      "Solomon Fokes",
+    ],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+  },
 ];
-const MIN_BOOK_RATING = 8;
+
 // Пиши код ниже этой строки
-
-const names = [...books]
-  .filter((book) => book.rating >= MIN_BOOK_RATING)
-  .map((book) => book.author)
-  .sort();
-
-console.log(names);
+const getNamesSortedByFriendCount = (users) =>
+  [...users]
+    .sort((curr, next) => curr.friends.length - next.friends.length)
+    .map((user) => user.name);
+// Пиши код выше этой строки
