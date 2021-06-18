ae44d6c97acd180a7857fbbc8f073bdf99ebc356
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 15:36:13 +0300

    30 Метод reduce()

--- js/script.js
+++ js/script.js
@@ -3,20 +3,19 @@
 // ------ TASK ------
 
 // Задание
-// Используя метод find() дополни код так, чтобы:
+// Игровому сервису необходим функционал подсчёта среднего времени проведённого в играх.
+// Дополни код так, чтобы в переменной totalPlayTime получилось общее игровое время из массива playtimes
 
-// В переменной bookWithTitle получился объект книги название которой (свойство title) совпадает со значением переменной BOOK_TITLE.
-// В переменной bookByAuthor получился объект книги автор который (свойство author) совпадает со значением переменной AUTHOR.
-
-const books = [
-  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
-  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
-  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
-  { title: "Красна как кровь", author: "Ли Танит", rating: 7.94 },
-];
-const BOOK_TITLE = "Сон смешного человека";
-const AUTHOR = "Роберт Шекли";
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244,
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
 // Пиши код ниже этой строки
 
-const bookWithTitle = books.find((book) => book.title === BOOK_TITLE);
-const bookByAuthor = books.find((book) => book.author === AUTHOR);
+const totalPlayTime = playtimes.reduce((acc, playtime) => acc + playtime, 0);
+
+// Пиши код выше этой строки
+const averagePlayTime = totalPlayTime / playtimes.length;
