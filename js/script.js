f8f7f8d9387f9f706213fbca114f9b2abcc34118
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 15:07:29 +0300

    22 Задача. Активные пользователи

--- js/script.js
+++ js/script.js
@@ -3,17 +3,82 @@
 // ------ TASK ------
 
 // Задание
-// Дополни функцию getUsersWithFriend(users, friendName) так, чтобы она возвращала массив пользователей у которых есть друг с именем в параметре friendName.
-// Массив друзей пользователя хранится в свойстве friends.
+// Дополни функцию getActiveUsers(users) так,
+// чтобы она возвращала массив активных пользователей, значение свойства isActive которых true.
+
+[
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39,
+  },
+];
 
 // Пиши код ниже этой строки
-const getFriends = (users) => {
-  const userAllFriends = users.flatMap((user) => user.friends);
-  return userAllFriends.filter(
-    (friend, index, array) => array.indexOf(friend) === index
-  );
-};
+const getActiveUsers = (users) => users.filter((user) => user.isActive);
 // Пиши код выше этой строки
-// const uniqueGenres = allGenres.filter(
-//     (book, index, array) => array.indexOf(book) === index
-//   );
