6be7b32e7df15b90cf3085a42e90932e8bec0a91
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 13:35:59 +0300

    15 Методы filter и find

--- js/script.js
+++ js/script.js
@@ -1,89 +1,9 @@
-// Этот массив объектов мы будем передавать в параметр users при вызове функции из задания.
+Задание;
+// Дополни код так, чтобы в переменной evenNumbers получился массив чётных чисел из массива numbers,
+// а в переменной oddNumbers массив нечётных. Обязательно используй метод filter().
 
-// [
-//   {
-//     name: 'Moore Hensley',
-//     email: 'moorehensley@indexia.com',
-//     eyeColor: 'blue',
-//     friends: ['Sharron Pace'],
-//     isActive: false,
-//     balance: 2811,
-//     skills: ['ipsum', 'lorem'],
-//     gender: 'male',
-//     age: 37,
-//   },
-//   {
-//     name: 'Sharlene Bush',
-//     email: 'sharlenebush@tubesys.com',
-//     eyeColor: 'blue',
-//     friends: ['Briana Decker', 'Sharron Pace'],
-//     isActive: true,
-//     balance: 3821,
-//     skills: ['tempor', 'mollit', 'commodo', 'veniam', 'laborum'],
-//     gender: 'female',
-//     age: 34,
-//   },
-//   {
-//     name: 'Ross Vazquez',
-//     email: 'rossvazquez@xinware.com',
-//     eyeColor: 'green',
-//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//     isActive: false,
-//     balance: 3793,
-//     skills: ['nulla', 'anim', 'proident', 'ipsum', 'elit'],
-//     gender: 'male',
-//     age: 24,
-//   },
-//   {
-//     name: 'Elma Head',
-//     email: 'elmahead@omatom.com',
-//     eyeColor: 'green',
-//     friends: ['Goldie Gentry', 'Aisha Tran'],
-//     isActive: true,
-//     balance: 2278,
-//     skills: ['adipisicing', 'irure', 'velit'],
-//     gender: 'female',
-//     age: 21,
-//   },
-//   {
-//     name: 'Carey Barr',
-//     email: 'careybarr@nurali.com',
-//     eyeColor: 'blue',
-//     friends: ['Jordan Sampson', 'Eddie Strong'],
-//     isActive: true,
-//     balance: 3951,
-//     skills: ['ex', 'culpa', 'nostrud'],
-//     gender: 'male',
-//     age: 27,
-//   },
-//   {
-//     name: 'Blackburn Dotson',
-//     email: 'blackburndotson@furnigeer.com',
-//     eyeColor: 'brown',
-//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//     isActive: false,
-//     balance: 1498,
-//     skills: ['non', 'amet', 'ipsum'],
-//     gender: 'male',
-//     age: 38,
-//   },
-//   {
-//     name: 'Sheree Anthony',
-//     email: 'shereeanthony@kog.com',
-//     eyeColor: 'brown',
-//     friends: ['Goldie Gentry', 'Briana Decker'],
-//     isActive: true,
-//     balance: 2764,
-//     skills: ['lorem', 'veniam', 'culpa'],
-//     gender: 'female',
-//     age: 39,
-//   },
-// ];
-
-// Задание
-// Дополни функцию getUserNames(users) так, чтобы она возвращала массив имён пользователей (свойство name)
-// из массива объектов в параметре users.
-
+const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
 // Пиши код ниже этой строки
-const getUserNames = (users) => users.map((user) => user.name);
-// Пиши код выше этой строки
+
+const evenNumbers = numbers.filter((number) => number % 2 === 0);
+const oddNumbers = numbers.filter((number) => number % 2 == !0);
