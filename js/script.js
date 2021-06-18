9406efd42af6b446a41aaaa80018d7e606a0b792
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 16:09:13 +0300

    35 Свой порядок сортировки чисел

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@
 
 // ------ TASK ------
 // Задание
-// Дополни функцию getTotalFriendCount(users) так, чтобы она считала и возвращала общее количество друзей
-// (свойство friends) всех пользователей из массива users. хранят пользователи из массива users.
+// Онлайн бибилиотеке необходимо отображать книги сортированные по дате издания, по её возрастанию или убыванию. Дополни код так, чтобы в переменной ascendingReleaseDates получилась отсортированная по возрастанию копия массива releaseDates,
+// а в переменной descendingReleaseDates копия отсортированная по убыванию.
 
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
 // Пиши код ниже этой строки
-const getTotalFriendCount = (users) =>
-  users.reduce((acc, user) => user.friends.length + acc, 0);
-// Пиши код выше этой строки
+
+const ascendingReleaseDates = [...releaseDates].sort();
+
+const descendingReleaseDates = [...releaseDates].sort(
+  (first, next) => next - first
+);
