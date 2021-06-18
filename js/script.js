e58ef137b9ca2d083796e8f56e6b38cf5894196f
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 15:55:07 +0300

    33 Задача. Общее количество друзей

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // -----
 
 // ------ TASK ------
-//Задание
-// Дополни функцию calculateTotalBalance(users) так, чтобы она считала и возвращала сумму всех средств (свойство balance)
-//которые хранят пользователи из массива users.
+// Задание
+// Дополни функцию getTotalFriendCount(users) так, чтобы она считала и возвращала общее количество друзей
+// (свойство friends) всех пользователей из массива users. хранят пользователи из массива users.
 
 // Пиши код ниже этой строки
-const calculateTotalBalance = (users) =>
-  users.reduce((acc, user) => acc + user.balance, 0);
+const getTotalFriendCount = (users) =>
+  users.reduce((acc, user) => user.friends.length + acc, 0);
 // Пиши код выше этой строки
