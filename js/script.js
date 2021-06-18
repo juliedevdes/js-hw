74a84ce92cdb9c4e68b2416b47aee83e57e1bb47
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 15:48:15 +0300

    31 Метод reduce() и массив объектов

--- js/script.js
+++ js/script.js
@@ -3,19 +3,22 @@
 // ------ TASK ------
 
 // Задание
-// Игровому сервису необходим функционал подсчёта среднего времени проведённого в играх.
-// Дополни код так, чтобы в переменной totalPlayTime получилось общее игровое время из массива playtimes
+// Нашему сервису необходимо рассчитать среднее время проведённое в одной игре для каждого игрока, и получить общую сумму этих времён. Рассчитать время для каждого из игроков,
+// можно разделив его время (свойство playtime) на количество игр (свойство gamesPlayed).
 
-const players = {
-  mango: 1270,
-  poly: 468,
-  ajax: 710,
-  kiwi: 244,
-};
-const playtimes = Object.values(players); // [1270, 468, 710, 244]
+const players = [
+  { name: "Манго", playtime: 1270, gamesPlayed: 4 },
+  { name: "Поли", playtime: 469, gamesPlayed: 2 },
+  { name: "Аякс", playtime: 690, gamesPlayed: 3 },
+  { name: "Киви", playtime: 241, gamesPlayed: 1 },
+];
 // Пиши код ниже этой строки
 
-const totalPlayTime = playtimes.reduce((acc, playtime) => acc + playtime, 0);
+const totalAveragePlaytimePerGame = players.reduce(
+  (acc, player) => acc + player.playtime / player.gamesPlayed,
+  0
+);
+// const totalPlayTime = playtimes.reduce((acc, playtime) => acc + playtime, 0);
 
-// Пиши код выше этой строки
-const averagePlayTime = totalPlayTime / playtimes.length;
+// // Пиши код выше этой строки
+// const averagePlayTime = totalPlayTime / playtimes.length;
