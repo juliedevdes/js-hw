eaa437f008719404ca2ee5187cc276e1a2f85980
Julia Ohrimenko <juliajam2497@gmail.com>
2021-06-18 16:24:17 +0300

    37 Сортировка объектов

--- js/script.js
+++ js/script.js
@@ -1,21 +1,35 @@
 // -----
 
 // ------ TASK ------
-// Задание
-// Онлайн бибилиотеке необходимо отображать книги отсортированные по автору, в алфавитном и обратном алфавитном порядке. Дополни код так, чтобы в переменной authorsInAlphabetOrder получилась отсортированная по алфавиту копия массива authors,
-// а в переменной authorsInReversedOrder копия отсортированная в обратном алфавитном порядке.
+// При работе с массивом объектов сортировка выполняется по числовому или строчному значению какого-то свойства.
+// Например, есть группа студентов с баллами за тест.
+// Необходимо отсортировать массив объектов по возрастанию и убыванию количества баллов, и по имени студента.
 
-const authors = [
-  "Ли Танит",
-  "Бернард Корнуэлл",
-  "Роберт Шекли",
-  "Федор Достоевский",
-  "Говард Лавкрафт",
+// Задание
+// Дополни код так, чтобы:
+
+// В переменной sortedByAuthorName получился массив книг отсортированный по имени автора в алфавитном порядке.
+// В переменной sortedByReversedAuthorName получился массив книг отсортированный по имени автора в обратном алфавитном порядке.
+// В переменной sortedByAscendingRating получился массив книг отсортированный по возрастанию рейтинга.
+// В переменной sortedByDescentingRating получился массив книг отсортированный по убыванию рейтинга.
+
+const books = [
+  { title: "Последнее королевство", author: "Бернард Корнуэлл", rating: 8.38 },
+  { title: "На берегу спокойных вод", author: "Роберт Шекли", rating: 8.51 },
+  { title: "Сон смешного человека", author: "Федор Достоевский", rating: 7.75 },
+  { title: "Красна как кровь", author: "Ли Танит", rating: 7.94 },
+  { title: "Враг Божий", author: "Бернард Корнуэлл", rating: 8.67 },
 ];
 // Пиши код ниже этой строки
 
-const authorsInAlphabetOrder = [...authors].sort();
-
-const authorsInReversedOrder = [...authors].sort((first, next) =>
-  next.localeCompare(first)
+const sortedByAuthorName = [...books].sort((a, b) =>
+  a.author.localeCompare(b.author)
 );
+
+const sortedByReversedAuthorName = [...books].sort((a, b) =>
+  b.author.localeCompare(a.author)
+);
+
+const sortedByAscendingRating = [...books].sort((a, b) => a.rating - b.rating);
+
+const sortedByDescentingRating = [...books].sort((a, b) => b.rating - a.rating);
